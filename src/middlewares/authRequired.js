c2980d619f5093d3d68d1119a5fa4277e8aeedf7
Duberly2004 <ivanmondragonmanchay@gmail.com>
2023-08-20 13:41:02 -0500

    Show cookie

--- src/middlewares/authRequired.js
+++ src/middlewares/authRequired.js
@@ -3,6 +3,17 @@ import { MySecretKey } from '../config.js'
 export const authRequired = (req,res,next) => {
     // is verified
     const {token} = req.cookies
+    console.log(req.cookies)
+    if(!token) return res.status(401).json({message:"No token, authorization denied"})
+    jwt.verify(token,MySecretKey, (err,user) => {
+        if(err) return res.status(401).json({message:"Invalid token"});
+            req.user = user
+        next()
+    })
+}
+
+export const VerifyToken = (req,res,next) => {
+    // is verified
     if(!token) return res.status(401).json({message:"No token, authorization denied"})
     jwt.verify(token,MySecretKey, (err,user) => {
         if(err) return res.status(401).json({message:"Invalid token"});
